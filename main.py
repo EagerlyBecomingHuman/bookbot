077b15d9437bac94d500447c0feef5123c80d129
EagerlyBecomingHuman <kjkaye7@gmail.com>
2024-05-30 11:22:07 -0500

    added function to convert characters to lowercase, count the characters, and print the character dictionary

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ def main():
 
     file_contents = open_file(path_to_file)
     word_count_in_file = count_words(file_contents)
+    character_count_in_file = count_characters(file_contents)
 
-    print(f"There are {word_count_in_file:,} words in this book.")
+    print(f"There are {word_count_in_file:,} words in this book. \n")
+    print(character_count_in_file)
 
 
 def open_file(path_to_file):
@@ -16,6 +18,17 @@ def open_file(path_to_file):
 def count_words(file_contents):
     book_of_words = file_contents.split()
     return len(book_of_words)
+
+
+def count_characters(file_contents):
+    character_dictionary = {}
+    for each_character in file_contents:
+        lowercase_characters = each_character.lower()
+        if lowercase_characters in character_dictionary:
+            character_dictionary[lowercase_characters] += 1
+        else:
+            character_dictionary[lowercase_characters] = 1
+    return character_dictionary
 
 
 main()
