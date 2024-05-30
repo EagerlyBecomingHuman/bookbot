773f8b2c47f8562ace738a4c0889da44f4a9cc08
EagerlyBecomingHuman <kjkaye7@gmail.com>
2024-05-30 10:43:49 -0500

    added function to count the words in the file

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 def main():
     path_to_file = "books/frankenstein.txt"
+
     file_contents = open_file(path_to_file)
-    print(file_contents)
+    word_count_in_file = count_words(file_contents)
+
+    # print(file_contents)
+    print(f"There are {word_count_in_file:,} words in this book.")
 
 
 def open_file(path_to_file):
     with open(path_to_file) as book:
         book_contents = book.read()
         return book_contents
+
+
+def count_words(file_contents):
+    book_of_words = file_contents.split()
+    return len(book_of_words)
 
 
 main()
