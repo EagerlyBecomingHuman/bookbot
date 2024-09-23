5d71c51b9ae7df2762ab72c246dfae304d248709
EagerlyBecomingHuman <kjkaye7@gmail.com>
2024-09-22 22:53:41 -0400

    removed print words and added print and count chars in new_feature_count_characters branch

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     file_path_to_book = "books/frankenstein.txt"
     entire_book = extract_book_contents(file_path_to_book)
     total_number_of_words = count_of_total_words(entire_book)
-    print(total_number_of_words)
+    char_count = count_chars_in_str(entire_book)
+    print(char_count)
 
 
 def extract_book_contents(file_path_to_book):
@@ -13,6 +14,17 @@ def extract_book_contents(file_path_to_book):
 def count_of_total_words(str_of_book_contents):
     word_count = str_of_book_contents.split()
     return len(word_count)
+
+
+def count_chars_in_str(str_of_book_contents):
+    char_dict = {}
+    for each_char in str_of_book_contents:
+        lower_case = each_char.lower()
+        if lower_case in char_dict:
+            char_dict[lower_case] += 1
+        else:
+            char_dict[lower_case] = 1
+    return char_dict
 
 
 main()
