4803a5f66840fd6932994c7549202d872fc4a1be
EagerlyBecomingHuman <kjkaye7@gmail.com>
2024-09-22 22:20:11 -0400

    update main to remove book print and add count words

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 def main():
     file_path_to_book = "books/frankenstein.txt"
     entire_book = extract_book_contents(file_path_to_book)
-    print(entire_book)
+    total_number_of_words = count_of_total_words(entire_book)
+    print(total_number_of_words)
 
 
 def extract_book_contents(file_path_to_book):
     with open(file_path_to_book) as f:
         return f.read()
+
+
+def count_of_total_words(str_of_book_contents):
+    word_count = str_of_book_contents.split()
+    return len(word_count)
 
 
 main()
